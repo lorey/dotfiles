29923fc9c7f9ca7bda5e8ffc5af28385f1f97797
lorey <git@karllorey.com>
2017-06-26 12:36:28 +0200

    AwesomeWM: open Thunar with strg-shift-enter

--- awesome/keys.lua
+++ awesome/keys.lua
@@ -39,6 +39,7 @@ globalkeys = awful.util.table.join(
 
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+    awful.key({ modkey, "Shift"   }, "Return", function () awful.util.spawn("thunar") end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
 
