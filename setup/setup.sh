6c265b49c52785903823127a679440c25237d69d
lorey <git@karllorey.com>
2018-08-25 21:48:25 +0200

    Setup script

---
+++ setup/setup.sh
@@ -0,0 +1,14 @@
+# refresh sudo credentials to make sure the authentication happens upfront
+sudo -v
+
+# redshift
+echo 'Installing redshift'
+sudo apt-get install redshift
+
+# vim
+echo 'Installing vim'
+sudo apt-get install vim
+
+# i3
+echo 'Installing i3'
+sudo apt-get install i3 i3status
